d55092d8d89592a664e1bca34b43fdc66811a2b4
Sam Oehlert <sam.oehlert@gmail.com>
2025-06-03 15:52:46 -0500

    refactor(future-proofing): allow for a position number so we can eventually try to build out the actual map of the circuit (what's connected to what in what order)
    also dont rqeuire a circuit number on creation of an outlet. i want to allow for creating a whole bunch of outlets then go through and assign them to circuits

--- tracker/models.py
+++ tracker/models.py
@@ -254,8 +254,9 @@ class Outlet(models.Model):
 
     device_type = models.CharField(max_length=50, choices=DeviceType.choices)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='outlets')
-    circuit = models.ForeignKey(Circuit, on_delete=models.CASCADE, related_name='outlets')
+    circuit = models.ForeignKey(Circuit, on_delete=models.CASCADE, blank=True, null=True, related_name='outlets')
     location_description = models.TextField(blank=True)
+    position_number = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
