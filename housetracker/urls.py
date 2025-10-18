be89597b8aa90aa63edb3cf515edfb3e22335c44
Sam Oehlert <sam.oehlert@gmail.com>
2025-10-18 15:01:31 -0500

    feat: Remove vehicle related files and settings from house app

--- housetracker/urls.py
+++ housetracker/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tracker.urls')),
+    path('', include('house.urls')),
 ]
 
 # Serve media files in development
