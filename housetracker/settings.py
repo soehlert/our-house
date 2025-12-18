c5756313182cded1f5b808b1b072453cfdfb922e
Sam Oehlert <sam.oehlert@gmail.com>
2025-12-17 23:47:07 -0600

    feat(vehicles): start adding vehicle tracker

--- housetracker/settings.py
+++ housetracker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'house.apps.HouseConfig',
+    'vehicles.apps.VehiclesConfig',
 ]
 
 MIDDLEWARE = [
--- housetracker/urls.py
+++ housetracker/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('house.urls')),
+    path('vehicles/', include('vehicles.urls')),
 ]
 
 # Serve media files in development
